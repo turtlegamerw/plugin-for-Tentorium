c20a17062cfa3b1416961c388e523431c5a70973
Turtlegamer_ <popperex9@gmail.com>
2024-08-11 22:28:14 +0200

    uhh still testing stuff
    making an test menu for the minigames!

--- src/main/java/me/turtle/plugintentorium/commands/PlayerMenuCommand.java
+++ src/main/java/me/turtle/plugintentorium/commands/PlayerMenuCommand.java
@@ -2,11 +2,16 @@ package me.turtle.plugintentorium.commands;
 
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
+import org.bukkit.Material;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+
+import java.util.List;
 
 public class PlayerMenuCommand implements CommandExecutor {
     @Override
@@ -14,8 +19,25 @@ public class PlayerMenuCommand implements CommandExecutor {
         if (sender instanceof Player player ) {
             //create an inventory with 9 slots with the name Tentorium test gui
             Inventory menu = Bukkit.createInventory(player, 9, ChatColor.AQUA + "Tentorium test gui");
-            
 
+            //clicky thing
+            ItemStack test = new ItemStack(Material.BARRIER);
+
+            //item nbt?
+            ItemMeta testMeta = test.getItemMeta();
+            testMeta.setDisplayName(ChatColor.GREEN + "Test");
+            testMeta.setLore(List.of(ChatColor.GOLD + "CRAZY LORE"));
+            test.setItemMeta(testMeta);
+
+
+            //adding items to test menu
+            menu.setItem(1, test);
+
+            //open Inventory
+            player.openInventory(menu);
+
+        }else{
+            sender.sendMessage(ChatColor.RED + "You must be a player to use this command.");
         }
         return true;
     }
