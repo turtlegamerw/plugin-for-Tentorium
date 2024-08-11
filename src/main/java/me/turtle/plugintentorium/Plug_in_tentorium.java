0d560b0c2c3fa5d7eee99542742f230e17326796
Turtlegamer_ <popperex9@gmail.com>
2024-08-11 12:10:56 +0200

    dsagagaadg

--- src/main/java/me/turtle/plugintentorium/Plug_in_tentorium.java
+++ src/main/java/me/turtle/plugintentorium/Plug_in_tentorium.java
@@ -2,25 +2,9 @@ package me.turtle.plugintentorium;
 
 import me.turtle.plugintentorium.Listeners.TnTRunListener;
 import me.turtle.plugintentorium.commands.*;
-import org.bukkit.Bukkit;
-import org.bukkit.Material;
-import org.bukkit.block.Block;
-import org.bukkit.entity.Player;
-import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.block.BlockPhysicsEvent;
-import org.bukkit.event.block.TNTPrimeEvent;
-import org.bukkit.event.player.PlayerMoveEvent;
 import org.bukkit.plugin.java.JavaPlugin;
-import org.bukkit.Location;
-import org.bukkit.World;
-import org.bukkit.entity.Player;
-import org.bukkit.plugin.java.JavaPlugin;
-import org.bukkit.potion.PotionEffect;
-import org.bukkit.potion.PotionEffectType;
 
-import static org.bukkit.Material.SAND;
-import static org.bukkit.Material.TNT;
 
 public class Plug_in_tentorium extends JavaPlugin implements Listener {
 
